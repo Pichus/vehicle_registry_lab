4fdab201435acbd7f537534ab1deadcc162c2097
pichus <dimatyzuk@gmail.com>
2026-09-24 11:45:44 +0200

    Fix string members of vehicle struct, make them arrays instead of pointers, specify max lengths

--- src/vehicle.h
+++ src/vehicle.h
@@ -2,12 +2,15 @@
 #define VEHICLE_H
 
 #include "person.h"
+#define LICENSE_PLATE_MAX_LENGTH 101
+#define BRAND_MAX_LENGTH 101
+#define TYPE_MAX_LENGTH 101
 
 struct Vehicle
 {
-    char *type;
-    char *brand;
-    char *license_plate;
+    char type[TYPE_MAX_LENGTH];
+    char brand[BRAND_MAX_LENGTH];
+    char license_plate[LICENSE_PLATE_MAX_LENGTH];
     struct Person owner;
 };
 
