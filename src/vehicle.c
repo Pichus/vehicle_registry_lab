24eefa4c999d45a934df4f102548933bf0e9a17a
pichus <dimatyzuk@gmail.com>
2026-09-25 09:23:20 +0200

    Introduce functions for printing vehicle structs and initializing it from user input

---
+++ src/vehicle.c
@@ -0,0 +1,48 @@
+#include "vehicle.h"
+#include "input_utilities.h"
+#include "person.h"
+#include <stdio.h>
+
+void print_vehicle(const struct Vehicle *vehicle)
+{
+    printf("Vehicle(type = \"%s\", brand = \"%s\", license_plate = \"%s\", "
+           "owner's name = \"%s\");\n",
+           vehicle->type, vehicle->brand, vehicle->license_plate,
+           vehicle->owner.name);
+}
+
+bool initialize_vehicle_from_user_input(struct Vehicle *vehicle)
+{
+    printf("Enter the vehicle's brand: ");
+    bool is_brand_input_successful =
+        read_user_input_into_buffer(vehicle->brand, BRAND_MAX_LENGTH);
+
+    if (!is_brand_input_successful)
+    {
+        return false;
+    }
+
+    printf("Enter the vehicle's license plate: ");
+    bool is_license_plate_input_successful = read_user_input_into_buffer(
+        vehicle->license_plate, LICENSE_PLATE_MAX_LENGTH);
+
+    if (!is_license_plate_input_successful)
+    {
+        return false;
+    }
+
+    printf("Enter the vehicle's type: ");
+    bool is_type_input_successful =
+        read_user_input_into_buffer(vehicle->type, TYPE_MAX_LENGTH);
+
+    if (!is_type_input_successful)
+    {
+        return false;
+    }
+
+    printf("Enter vehicle owner's details:\n");
+    bool is_owner_input_successful =
+        initialize_person_from_user_input(&(vehicle->owner));
+
+    return is_owner_input_successful;
+}
--- src/vehicle.h
+++ src/vehicle.h
@@ -2,6 +2,7 @@
 #define VEHICLE_H
 
 #include "person.h"
+#include <stdbool.h>
 #define LICENSE_PLATE_MAX_LENGTH 101
 #define BRAND_MAX_LENGTH 101
 #define TYPE_MAX_LENGTH 101
@@ -13,5 +14,9 @@ struct Vehicle
     char license_plate[LICENSE_PLATE_MAX_LENGTH];
     struct Person owner;
 };
+
+void print_vehicle(const struct Vehicle *vehicle);
+
+bool initialize_vehicle_from_user_input(struct Vehicle *vehicle);
 
 #endif // VEHICLE_H
