ea7d5abd94bd56045b93b477370ff0710ed0c2bf
pichus <dimatyzuk@gmail.com>
2026-09-25 14:01:09 +0200

    Create first draft of add vehicle and show all vehicles operations

--- src/main.c
+++ src/main.c
@@ -1,74 +1,73 @@
-#include "menu_option.h"
+#include "input_utilities.h"
+#include "menu.h"
 #include "operation.h"
-#include "person.h"
+#include "registry.h"
 #include "vehicle.h"
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define OPERATION_SELECTION_LOWER_BOUND 0
-#define OPERATION_SELECTION_UPPER_BOUND 7
-#define MAX_USER_INPUT_BUFFER_SIZE 101
-#define INPUT_NUMERICAL_BASE 10
-
-void display_menu(struct MenuOption menu_options[], int menu_options_size)
+bool handle_add_vehicle_operation(struct Registry *registry)
 {
-    for (int i = 0; i < menu_options_size; i++)
+    if (is_registry_full(registry))
     {
-        printf("%d. %s\n", menu_options[i].operation, menu_options[i].description);
+        printf("Vehicle registry is already full\n");
+        return false;
     }
-}
 
-bool is_menu_option_choice_valid(long menu_option_choice)
-{
-    return menu_option_choice >= OPERATION_SELECTION_LOWER_BOUND &&
-           menu_option_choice <= OPERATION_SELECTION_UPPER_BOUND;
-}
+    struct Vehicle vehicle;
+    bool is_vehicle_initialization_successful =
+        initialize_vehicle_from_user_input(&vehicle);
 
-bool read_user_input(long *user_input)
-{
-
-    char user_input_buffer[MAX_USER_INPUT_BUFFER_SIZE];
-    if (fgets(user_input_buffer, MAX_USER_INPUT_BUFFER_SIZE, stdin) == NULL)
+    if (!is_vehicle_initialization_successful)
     {
         return false;
     }
 
-    char *end_ptr;
+    bool is_push_back_successful =
+        push_back_vehicle_to_registry(registry, vehicle);
 
-    errno = 0;
-    long parsed_input = strtol(user_input_buffer, &end_ptr, INPUT_NUMERICAL_BASE);
-
-    bool input_out_of_long_type_range = errno == ERANGE;
-    bool no_digits_found = end_ptr == user_input_buffer;
-    bool found_further_chars_after_number = *end_ptr != '\0' && *end_ptr != '\n';
-    if (input_out_of_long_type_range || no_digits_found || found_further_chars_after_number)
+    if (!is_push_back_successful)
     {
+        printf("Vehicle registry is already full\n");
         return false;
     }
-
-    *user_input = parsed_input;
 
     return true;
 }
 
-void run_main_loop()
+bool perform_operation_on_registry(struct Registry *registry,
+                                   enum Operation operation)
 {
-    struct MenuOption menu_options[] = {{.operation = ADD_VEHICLE, .description = "Add vehicle"},
-                                        {.operation = REMOVE_VEHICLE, .description = "Remove vehicle"},
-                                        {.operation = SORT_REGISTRY, .description = "Sort"},
-                                        {.operation = SHOW_INFO_FOR_VEHICLE, .description = "Info"},
-                                        {.operation = SHOW_INFO_FOR_ALL_VEHICLES, .description = "Show all"},
-                                        {.operation = ADD_RANDOM_VEHICLE, .description = "Add random"},
-                                        {.operation = SEARCH_FOR_VEHICLE_OWNER, .description = "Search"},
-                                        {.operation = QUIT, .description = "Quit"}};
-    int running = 1;
+    bool result = true;
+
+    switch (operation)
+    {
+    case ADD_VEHICLE:
+        result = handle_add_vehicle_operation(registry);
+        break;
+    case SHOW_INFO_FOR_ALL_VEHICLES:
+        print_registry_contents(registry);
+        break;
+    default:
+        break;
+    }
+
+    return result;
+}
+
+void run_main_loop(void)
+{
+    struct Registry registry;
+    initialize_registry_with_default_values(&registry);
+
+    bool running = true;
     while (running)
     {
-        display_menu(menu_options, 8);
-        long user_input;
-        bool read_user_input_successfully = read_user_input(&user_input);
+        display_menu();
+        int user_input;
+        bool read_user_input_successfully = read_int_input(&user_input);
 
         if (!read_user_input_successfully)
         {
@@ -78,21 +77,25 @@ void run_main_loop()
 
         if (!is_menu_option_choice_valid(user_input))
         {
-            printf("Invalid menu choice: the choice must be in range [%d, %d]\n", OPERATION_SELECTION_LOWER_BOUND,
-                   OPERATION_SELECTION_UPPER_BOUND);
+            printf(
+                "Invalid menu choice: the choice must be in range [%d, %d]\n",
+                OPERATION_SELECTION_LOWER_BOUND,
+                OPERATION_SELECTION_UPPER_BOUND);
             continue;
         }
 
         enum Operation next_operation = user_input;
 
+        perform_operation_on_registry(&registry, next_operation);
+
         if (next_operation == QUIT)
         {
-            running = 0;
+            running = false;
         }
     }
 }
 
-int main()
+int main(void)
 {
     run_main_loop();
     return 0;
